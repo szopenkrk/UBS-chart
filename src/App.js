257a9f2af90d58fdaf0556de840ce3e17c46f6cf
admin <admin@MacBook-Pro-admin.local>
2018-07-09 01:10:02 +0200

    Add methods to show data and flipp value

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import CurrencyChart from './components/currency-chart/index';
-// import data from './mock/data.json'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currency : [
+                {"pair":"USD CHF", "buy":0.99143, "sell":0.99043},
+                {"pair":"GBP USD", "buy":1.28495, "sell":1.2836},
+                {"pair":"GBP CHF", "buy":1.27378, "sell":1.27147},
+                {"pair":"EUR SEK", "buy":9.632, "sell":9.6055},
+                {"pair":"USD JPY", "buy":110.467, "sell":110.417},
+                {"pair":"EUR JPY", "buy":120.589, "sell":120.491}
+            ]
+        }
+    }
+
+  componentDidMount () {
+      setInterval(() => this.updateDate(), 1000);
+      this.forceUpdate()
+  }
+
+  updateDate () {
+      console.log('Update data');
+      this.state.currency.map(function(cash, index) {
+          var counter = Math.floor(Math.random() * 2) + 1;
+          var counter2 = Math.floor(Math.random() * 2) + 1;
+          cash.buy = counter === 1 ? cash.buy +1: cash.buy-1;
+          cash.sell = counter2 === 1 ? cash.sell +1: cash.sell-1;
+      });
+      this.forceUpdate()
+  }
+
   render() {
     return (
       <div className="App">
-        <CurrencyChart currency="USD CHF" data={data}/>
-        <CurrencyChart currency="GBP USD" data={data}/>
-        <CurrencyChart currency="EUR CAD" data={data}/>
+      	{
+      		this.state.currency.map(
+      			function(cash, index) { 
+      				return <CurrencyChart key={index} currency={cash.pair} buy={cash.buy} sell={cash.sell} />
+      			}
+      		)
+      	}
+        
       </div>
     );
   }
--- src/components/currency-chart/index.js
+++ src/components/currency-chart/index.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import styles from './index.css';
+import { styles } from './index.css';
 
 class CurrencyChart extends Component {
-	constructor(props){
-    	super(props);
-
-    }
-
 	componentWillMount () {
 		console.log('Mounted');
+	}
+
+	renderArrow (buy ,sell) {
+		if(buy > sell) {
+			return 'redArrow';
+		} else {
+			return 'greenArrow';
+		}
+	}
+
+	showPartOfPair (currency, part) {
+		if(part === 1) {
+			return currency.substring(0,3);
+		}
+		if(part === 2) {
+			return currency.substring(4,7);
+		}
 	}
 
 	render() {
@@ -18,8 +30,24 @@ class CurrencyChart extends Component {
     				{this.props.currency}
     			</div>
     			<div className="leftCompare">
+    				<div className="buyHeader">
+    					Buy {this.showPartOfPair(this.props.currency, 1)}
+    				</div>
+    				<div className="buyValue">
+    					{this.props.buy}
+    				</div>
+    			</div>
+    			<div className="centerArrow">
+    				<div className={this.renderArrow(this.props.buy, this.props.sell)}>
+    				</div>
     			</div>
     			<div className="rightCompare">
+    				<div className="sellHeader">
+    					Sell {this.showPartOfPair(this.props.currency, 2)}
+    				</div>
+    				<div className="sellValue">
+    					{this.props.sell}
+    				</div>
     			</div>
     		</div>
     	)
