d5f45e96dd912748d4cb346ad13485f30e3c74d1
admin <admin@MacBook-Pro-admin.local>
2018-07-08 13:04:16 +0200

    Add structure and first commit

---
+++ src/components/currency-chart/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from './style.css';
+
+class CurrencyChart extends Component {
+	render() {
+    	return (
+    		<div id="currencyContainer">
+    			<div className={styles.currencyHeader}>
+    			</div>
+
+    		</div>
+    	)
+	}
+}
+
+export default CurrencyChart;
